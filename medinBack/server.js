af1e44cd4890580da125e84847c7f64b18694d51
yassinht <yassinhtt@gmail.com>
2023-03-19 15:34:33 +0100

    test

--- medinBack/server.js
+++ medinBack/server.js
@@ -1,27 +1,14 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express=require('express');
+const app=express();
 
-const app = express();
-
-// Maintenance mode middleware
-app.use((req, res, next) => {
-  if (fs.existsSync('/var/www/maintenance.flag')) {
-    res.status(503).sendFile(path.join(__dirname, 'maintenance.html'));
-  } else {
-    next();
-  }
-});
-
-// Serve static files
 app.use(express.static('dist/medIn/'));
 
-// Serve index.html for all routes
-app.get('*', function(req, res) {
-  res.sendFile(path.resolve('dist/medIn/index.html'));
+app.get('*', function(req,res) {
+    res.sendFile(path.resolve('dist/medIn/index.html'));
 });
 
-// Start the server
-app.listen(7000, () => {
-  console.log('App running on port 7000');
-});
+
+
+app.listen(7000,()=>{
+    console.log('app running on 7000')
+})
